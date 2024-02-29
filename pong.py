34d86909f41a8c2b82e89931998363211714bd54
Ekaterina Zavyalova <ekaterina.zavyalova@tuni.fi>
2024-02-29 13:24:31 +0200

    ball and gates collission

--- pong.py
+++ pong.py
@@ -34,6 +34,9 @@ ball.shape("circle")
 ball.color("white")
 ball.penup()
 ball.goto(0, 30)
+ball.xm = -2
+ball.ym = -2
+
 
 # Moves
 # gate_1 up-down
@@ -72,8 +75,6 @@ wn.onkeypress(gate_2_up, "Up")
 wn.onkeypress(gate_2_down, "Down")   
 
 
-
-
 # Main game loop
 running = True
 def on_close():
@@ -86,6 +87,42 @@ wn.listen()
 while running:
     wn.update()
     time.sleep(0.017)  # Sleep for roughly 60 frames per second
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.xm)
+    ball.sety(ball.ycor() + ball.ym)
+
+    # Border checking
+    # Up and down
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.ym *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.ym *= -1    
+
+    # Left and right
+    if ball.xcor() > 390:
+        ball.goto(0,0)
+        ball.xm *= -1
+    
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.xm *= -1
+
+    # Ball and gates
+        
+    if ball.xcor() > 323 and ball.xcor() < 340 and (ball.ycor() < gate_2.ycor() + 30 and ball.ycor() > gate_2.ycor() - 30):
+        ball.setx(323)
+        ball.xm *= -1
+    #     ball.setx(-330)
+    #     ball.xm *= -1
+
+    # if ball.xcor() == gate_2.xcor():s
+    #     ball.setx(320)
+    #     ball.xm *= -1    
+        
 
 # Close the window gracefully
 wn.bye()
